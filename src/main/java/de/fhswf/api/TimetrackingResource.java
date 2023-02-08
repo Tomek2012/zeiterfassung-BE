a6c2d8911391b868b44dba5c853291c110bcc29e
Tomek2012 <Tomek2012@users.noreply.github.com>
2023-02-08 23:07:47 +0100

    Erweiterung Backend - Speichern und Loeschen von Zeiterfassungen

--- src/main/java/de/fhswf/api/TimetrackingResource.java
+++ src/main/java/de/fhswf/api/TimetrackingResource.java
@@ -2,44 +2,64 @@ package de.fhswf.api;
 
 import de.fhswf.entities.Timetracking;
 import io.quarkus.security.Authenticated;
+import org.eclipse.microprofile.jwt.Claims;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.jboss.logging.Logger;
 
-import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.List;
 
-@Path("/api")
+@Path("/time")
+@Authenticated
 public class TimetrackingResource {
 
+    private static final Logger LOG = Logger.getLogger(TimetrackingResource.class);
+
+    @Inject
+    JsonWebToken jwt;
+
     @GET
-    @Path("time")
     @Produces(MediaType.APPLICATION_JSON)
-    @Authenticated
-    public Response getAll() {
-        List<Timetracking> times = Timetracking.listAll();
+    public Response getAllTimerackings() {
+        String test = jwt.getClaim(Claims.sub);
+        List<Timetracking> times = Timetracking.list("userId", test);
         return Response.ok(times).build();
     }
 
     @POST
-    @Path("time")
+    @Path("/save")
     @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createTime(Timetracking timetrackings) {
-        Timetracking.persist(timetrackings);
-        if (timetrackings.isPersistent()) {
-            return Response.created(URI.create("/time" + timetrackings.id)).build();
+    public Response saveAndUpdate(List<Timetracking> timetracking) {
+        try {
+            for (int i = 0; i < timetracking.size();
+                 i++) {
+                if (timetracking.get(i).id == null) {
+                    timetracking.get(i).userId = jwt.getClaim(Claims.sub);
+                    Timetracking.persist(timetracking.get(i));
+                } else {
+                    Timetracking entity = Timetracking.findById(timetracking.get(i).id);
+                    entity.fromTime = timetracking.get(i).fromTime;
+                    entity.toTime = timetracking.get(i).toTime;
+                    entity.workingpackage = timetracking.get(i).workingpackage;
+                    entity.description = timetracking.get(i).description;
+                }
+            }
+            return Response.ok(Timetracking.listAll()).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST.getStatusCode(), e.getMessage()).build();
         }
-        return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
     @DELETE
     @Transactional
-    @Path("time/{id}")
+    @Path("{id}")
     public Response deleteById(@PathParam("id") Long id) {
         boolean deleted = Timetracking.deleteById(id);
         if (deleted) {
@@ -48,11 +68,17 @@ public class TimetrackingResource {
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
-    @GET
-    @Path("hellosicher")
-    @Produces(MediaType.TEXT_PLAIN)
-    @RolesAllowed("time_administration")
-    public String share() {
-        return "Hello productlist share";
+    // Nur zum Hinzufuegen neuer Zeiterfassungen (Testzwecke) - FE benutzt saveAndUpdate()
+    @POST
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response saveTime(Timetracking timetracking) {
+        timetracking.userId = jwt.getClaim(Claims.sub);
+        Timetracking.persist(timetracking);
+        if (timetracking.isPersistent()) {
+            return Response.created(URI.create("/time" + timetracking.id)).build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
     }
 }
---
+++ src/main/java/de/fhswf/api/WorkingpackageResource.java
@@ -0,0 +1,28 @@
+package de.fhswf.api;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.jboss.logging.Logger;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/workingpackage")
+public class WorkingpackageResource {
+
+    private static final Logger LOG = Logger.getLogger(TimetrackingResource.class);
+
+    @Inject
+    JsonWebToken jwt;
+
+    @GET
+    @Path("hellosicher")
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed("time_administration")
+    public String share() {
+        return "Hello productlist share";
+    }
+}
